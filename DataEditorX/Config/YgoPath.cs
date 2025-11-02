264fffc785a82ad8d99136b220e74948c0e3f738
salix5 <salix5566@gmail.com>
2025-11-02 20:39:38 +0800

    Refactor YgoPath class

--- DataEditorX/Config/YgoPath.cs
+++ DataEditorX/Config/YgoPath.cs
@@ -4,26 +4,23 @@ namespace DataEditorX.Config
 {
     public class YgoPath
     {
-        public YgoPath(string gamepath)
+        public YgoPath(string _gamepath)
         {
-            this.SetPath(gamepath);
+            SetPath(_gamepath);
         }
-        public void SetPath(string gamepath)
+        public void SetPath(string _gamepath)
         {
-            this.gamepath = gamepath;
-            this.picpath = MyPath.Combine(gamepath, "pics");
-            this.fieldpath = MyPath.Combine(this.picpath, "field");
-            this.picpath2 = MyPath.Combine(this.picpath, "thumbnail");
-            this.luapath = MyPath.Combine(gamepath, "script");
-            this.ydkpath = MyPath.Combine(gamepath, "deck");
-            this.replaypath = MyPath.Combine(gamepath, "replay");
+            gamepath = _gamepath;
+            picpath = MyPath.Combine(gamepath, "pics");
+            fieldpath = MyPath.Combine(picpath, "field");
+            luapath = MyPath.Combine(gamepath, "script");
+            ydkpath = MyPath.Combine(gamepath, "deck");
+            replaypath = MyPath.Combine(gamepath, "replay");
         }
         /// <summary>游戏目录</summary>
         public string gamepath;
         /// <summary>大图目录</summary>
         public string picpath;
-        /// <summary>小图目录</summary>
-        public string picpath2;
         /// <summary>场地图目录</summary>
         public string fieldpath;
         /// <summary>脚本目录</summary>
@@ -35,63 +32,53 @@ namespace DataEditorX.Config
 
         public string GetImage(long id)
         {
-            return this.GetImage(id.ToString());
+            return GetImage(id.ToString());
         }
-        //public string GetImageThum(long id)
-        //{
-        //	return GetImageThum(id.ToString());
-        //}
         public string GetImageField(long id)
         {
-            return this.GetImageField(id.ToString());//场地图
+            return GetImageField(id.ToString());//场地图
         }
         public string GetScript(long id)
         {
-            return this.GetScript(id.ToString());
+            return GetScript(id.ToString());
         }
         public string GetYdk(string name)
         {
-            return MyPath.Combine(this.ydkpath, name + ".ydk");
+            return MyPath.Combine(ydkpath, name + ".ydk");
         }
         //字符串id
         public string GetImage(string id)
         {
             return MyPath.Combine(this.picpath, id + ".jpg");
         }
-        //public string GetImageThum(string id)
-        //{
-        //	return MyPath.Combine(picpath2, id + ".jpg");
-        //}
         public string GetImageField(string id)
         {
-            return MyPath.Combine(this.fieldpath, id + ".png");//场地图
+            return MyPath.Combine(fieldpath, id + ".png");//场地图
         }
         public string GetScript(string id)
         {
-            return MyPath.Combine(this.luapath, "c" + id + ".lua");
+            return MyPath.Combine(luapath, "c" + id + ".lua");
         }
         public string GetModuleScript(string modulescript)
         {
-            return MyPath.Combine(this.luapath, modulescript + ".lua");
+            return MyPath.Combine(luapath, modulescript + ".lua");
         }
 
         public string[] GetCardfiles(long id)
         {
             string[] files = new string[]{
-                this.GetImage(id),//大图
-                //GetImageThum(id),//小图
-                this.GetImageField(id),//场地图
-                this.GetScript(id)
+                GetImage(id),//大图
+                GetImageField(id),//场地图
+                GetScript(id)
            };
             return files;
         }
         public string[] GetCardfiles(string id)
         {
             string[] files = new string[]{
-                this.GetImage(id),//大图
-                //GetImageThum(id),//小图
-                this.GetImageField(id),//场地图
-                this.GetScript(id)
+                GetImage(id),//大图
+                GetImageField(id),//场地图
+                GetScript(id)
            };
             return files;
         }
