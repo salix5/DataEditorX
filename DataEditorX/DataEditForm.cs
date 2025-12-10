40c3ca499862aaa19daf17420e543bf4de1b223f
salix5 <salix5566@gmail.com>
2025-12-10 16:09:32 +0800

    Remove unused DataEditForm_Load event handler

--- DataEditorX/DataEditForm.cs
+++ DataEditorX/DataEditForm.cs
@@ -2141,11 +2141,6 @@ namespace DataEditorX
             }
         }
 
-        private void DataEditForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void menuitem_language_Click(object sender, EventArgs e)
         {
 
