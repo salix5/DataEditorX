5cf77d8e480510672dd1762a50cd0bb1a23dc5ae
salix5 <salix5566@gmail.com>
2025-12-16 18:27:50 +0800

    Revert "using HashCode.Combine in GetHashCode"
    This reverts commit 59a0aedbe5a8de7eb16b662e9c9b50e6434a3476.

--- DataEditorX/Core/Card.cs
+++ DataEditorX/Core/Card.cs
@@ -286,7 +286,12 @@ namespace DataEditorX.Core
         /// <summary>
         /// 得到哈希值
         /// </summary>
-        public override int GetHashCode() => HashCode.Combine(id, name, type);
+        public override int GetHashCode()
+        {
+            // combine the hash codes of all members here (e.g. with XOR operator ^)
+            int hashCode = id.GetHashCode() + name.GetHashCode();
+            return hashCode;//member.GetHashCode();
+        }
         /// <summary>
         /// 比较卡片是否相等
         /// </summary>
