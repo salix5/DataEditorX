5a97c3996a846d3a371c5aed03fd14793c3dfb21
salix5 <salix5566@gmail.com>
2025-12-16 18:49:19 +0800

    Simplify Combine method by using Path.Combine

--- DataEditorX/Common/MyPath.cs
+++ DataEditorX/Common/MyPath.cs
@@ -36,42 +36,7 @@ namespace System.IO
         /// <returns></returns>
         public static string Combine(params string[] paths)
         {
-            if (paths.Length == 0)
-            {
-                throw new ArgumentException("please input path");
-            }
-            else
-            {
-                StringBuilder builder = new();
-                string spliter = Path.DirectorySeparatorChar.ToString();
-                string firstPath = paths[0];
-                if (!firstPath.EndsWith(spliter))
-                {
-                    firstPath += spliter;
-                }
-                builder.Append(firstPath);
-                for (int i = 1; i < paths.Length; i++)
-                {
-                    string nextPath = paths[i];
-                    if (nextPath.StartsWith("/") || nextPath.StartsWith("\\"))
-                    {
-                        nextPath = nextPath.Substring(1);
-                    }
-                    if (i != paths.Length - 1)//not the last one
-                    {
-                        if (nextPath.EndsWith("/") || nextPath.EndsWith("\\"))
-                        {
-                            nextPath = nextPath.Substring(0, nextPath.Length - 1) + spliter;
-                        }
-                        else
-                        {
-                            nextPath += spliter;
-                        }
-                    }
-                    builder.Append(nextPath);
-                }
-                return builder.ToString();
-            }
+            return Path.Combine(paths);
         }
         /// <summary>
         /// 检查目录是否合法
