ad160f1bdc31d2d204f925962a568bd35322dc00
salix5 <salix5566@gmail.com>
2025-12-24 15:05:58 +0800

    Refactor: change DefaultSQL and DefaultTableSQL to private

--- DataEditorX/Core/Database.cs
+++ DataEditorX/Core/Database.cs
@@ -21,11 +21,11 @@ namespace DataEditorX.Core
     public static class Database
     {
         #region SQL statements
-        public const string DefaultSQL =
+        const string DefaultSQL =
             "SELECT id,datas.ot,datas.alias,datas.setcode,datas.type,datas.atk,datas.def,datas.level,datas.race,datas.attribute,datas.category,"
             + "texts.name,texts.desc,texts.str1,texts.str2,texts.str3,texts.str4,texts.str5,texts.str6,texts.str7,texts.str8,texts.str9,texts.str10,"
             + "texts.str11,texts.str12,texts.str13,texts.str14,texts.str15,texts.str16 FROM datas JOIN texts USING(id) WHERE 1 = 1";
-        public const string DefaultTableSQL =
+        const string DefaultTableSQL =
             "CREATE TABLE datas(id INTEGER PRIMARY KEY, ot INTEGER, alias INTEGER, setcode INTEGER, type INTEGER, atk INTEGER, def INTEGER, level INTEGER, race INTEGER, attribute INTEGER, category INTEGER);\n"
             + "CREATE TABLE texts(id INTEGER PRIMARY KEY, name TEXT, desc TEXT, str1 TEXT, str2 TEXT, str3 TEXT, str4 TEXT, str5 TEXT, str6 TEXT, str7 TEXT, str8 TEXT, str9 TEXT, str10 TEXT,"
             + " str11 TEXT, str12 TEXT, str13 TEXT, str14 TEXT, str15 TEXT, str16 TEXT);";
